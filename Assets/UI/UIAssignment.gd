1f043e21e33fb63d4869a7daf63815f719d5fd2c
iffn <iffn1990@gmail.com>
2026-08-07 10:13:24 +0200

    Improved grass displays and implemented hovering

--- Assets/UI/UIAssignment.gd
+++ Assets/UI/UIAssignment.gd
@@ -8,11 +8,13 @@ extends Node
 @export var meshToDisplay : Mesh
 @export var display_scale := 0.3
 @export var euler_angles := Vector3(0, 0,  0)
+@export var hovering := true
 
 @export_category("Prefab settings")
 @export var title_label : Label
 @export var description_label : Label
 @export var displayMesh : MeshInstance3D
+@export var displayOrigin : Node3D
 
 @export_category("Apply")
 @export var click_to_apply : bool:
@@ -33,6 +35,7 @@ func apply_data() -> void:
 	displayMesh.set_surface_override_material(0, material)
 	displayMesh.scale = display_scale * Vector3.ONE
 	displayMesh.rotation = euler_angles * deg_to_rad(1)
+	displayOrigin.position.y = 1.3 if hovering else 1.001
 
 func _ready() -> void:
 	apply_data()
